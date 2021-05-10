a03be8cfe7aaebcb5f8ce27bf2a22650405d258b
Joshua Lokken <joshualokken@pm.me>
2021-05-10 11:49:41 -0500

    React tooltip component and basic sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.scss";
+import ToolTip from "./ToolTipComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToolTip description={"textual information"} direction={"right"} />
+      <ToolTip
+        description={"Some random text asdfsdf asdfasdf asdfasdf"}
+        direction={"left"}
+      />
+      <ToolTip
+        description={"Some random text asdfsdf asdfasdf asdfasdf"}
+        direction={"top"}
+      />
+      <ToolTip
+        description={"Some random text asdfsdf asdfasdf asdfasdf"}
+        direction={"bottom"}
+      />
     </div>
   );
 }
---
+++ src/ToolTipComponent.jsx
@@ -0,0 +1,26 @@
+const ToolTip = ({ description, direction, className }) => {
+  const getDirection = () => {
+    const appendedClass = className ? className : "";
+    switch (direction) {
+      case "top":
+      case "right":
+      case "left":
+        return `${direction} ${appendedClass}`;
+      default:
+        return `right ${appendedClass}`;
+    }
+  };
+
+  return (
+    <div className={`tooltip-wrapper ${getDirection()}`}>
+      <img
+        src="https://img.icons8.com/color/50/000000/info.png"
+        className="icon"
+        alt="icon"
+      />
+      <div className="description">{description}</div>
+    </div>
+  );
+};
+
+export default ToolTip;
